a4ed412c4f45a7a74b1d9e44673523b0d64acf6e
nilamdarmataksiah <107944692+nilamdarmataksiah@users.noreply.github.com>
2022-06-24 17:17:07 +0700

    Add files via upload

---
+++ Segitiga_model.php
@@ -0,0 +1,33 @@
+<?php
+ 	class Segitiga_model extends CI_Model {
+ 		// mendefinisikan konstanta untuk nilai PI
+ 			const ST = 0.5;
+ 		// atribut model
+ 			private $alas;
+ 			// metode untuk menentukan nilai $radius
+ 				public function set_alas($a) {
+ 				$this->alas = $a;
+ 				}
+ 			// metode untuk mengambil nilai $radius
+ 				public function get_alas() {
+ 				return $this->alas;
+ 				}
+ 			private $tinggi;
+ 			// metode untuk menentukan nilai $radius
+ 				public function set_tinggi($t) {
+ 				$this->tinggi = $t;
+ 				}
+ 			// metode untuk mengambil nilai $radius
+ 				public function get_tinggi() {
+ 				return $this->tinggi;
+ 				}
+		// metode untuk menghitung luas lingkaran
+ 			public function hitung_luas() {
+ 			return self::ST * $this->alas * $this->tinggi;
+ 			}
+ // metode untuk menghitung keliling lingkaran
+ //public function hitung_keliling() {
+ //return 2 * self::PI * $this->radius;
+ //}
+ }
+?>
---
+++ segitiga_form_view.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+ 		<title>Demo Model</title>
+ 	</head>
+ 	<body>
+ 		<h2>Luas Segitiga</h2>
+ 		<form action="http://localhost/ci/index.php/segitiga" method="post">
+ 			Masukkan nilai alas:<br/>
+ 			<input type="text" name="alas1">
+ 			<br>Masukkan nilai tinggi:<br/>
+ 			<input type="text" name="tinggi1"> <br/>
+ 			<input type="submit" name="btnSubmit" value="Hitung">
+ 		</form>
+ 	</body>
+</html>
